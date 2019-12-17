6e8dfd11f005239ffa28f3bb2d90ed8dfe5096b0
Chris Sketch <christopher.sketch@gm.com>
2019-12-17 00:48:41 -0500

    bad qsort

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,9 @@ fn main() {
     println!("Before {:?}", v);
     sorting::insertion_sort(&mut v);
     println!("After  {:?}", v);
+
+    v = vec![0_i32, 0, 0, 0, 0];
+    let len = v.len();
+    sorting::quick_sort(&mut v, 0, len - 1);
+    assert_eq!(v, [-2_i32, -1, 0, 1, 2]);
 }
