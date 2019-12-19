9f55659e91a9e056c8b31155de4c180d3f4ab153
Chris Sketch <sketchc89@gmail.com>
2019-12-18 21:26:27 -0500

    impl drop

--- src/first_ll.rs
+++ src/first_ll.rs
@@ -40,6 +40,15 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut link = std::mem::replace(&mut self.head, Link::Terminating);
+        while let Link::Connecting(mut _node) = link {
+            link = std::mem::replace(&mut self.head, Link::Terminating);
+        }
+    }
+}
+
 #[cfg(test)]
 mod testing {
     use super::*;
