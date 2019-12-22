cc2708115cba0ce408d6eec47793935fe654b3ee
Chris Sketch <sketchc89@gmail.com>
2019-12-21 21:11:57 -0500

    access operator[]

--- cpp/dataStructures/include/dataStructures/Vector.h
+++ cpp/dataStructures/include/dataStructures/Vector.h
@@ -4,7 +4,7 @@ namespace ds {
 
 /*! @brief Resizable array */
 template <typename T>
-class Vector<T> {
+class Vector {
 public:
     /*! @brief Default constructor */
     Vector();
@@ -12,11 +12,18 @@ public:
     Vector(size_t size);
     /*! @brief Construct resizable array of given size initialized to given value
      */
-    Vector(size_t size, T val);
+    Vector(size_t size, const T& val);
+
+    /*! @brief access operator */
+    const T& operator[](size_t idx) const;
 
 private:
+    T _array[0];
+    size_t _used;
+    size_t _reserved;
 };
 
 // impl
 #include "Vector.hpp"
+
 }  // namespace ds
--- cpp/dataStructures/include/dataStructures/Vector.hpp
+++ cpp/dataStructures/include/dataStructures/Vector.hpp
@@ -1,1 +1,17 @@
-Vector::Vector() {}
+/*! @file Vector.hpp
+ *  @brief Implements templated Vector data structure */
+
+template <typename T>
+Vector<T>::Vector() {}
+
+template <typename T>
+Vector<T>::Vector(size_t size) {}
+
+template <typename T>
+Vector<T>::Vector(size_t size, const T& val) {}
+
+template <typename T>
+const T& Vector<T>::operator[](size_t idx) const {
+    assert(idx < _used);
+    return _array[idx];
+}
